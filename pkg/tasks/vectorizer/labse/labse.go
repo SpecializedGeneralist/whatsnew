f2f8b97c48eea03ac2b194a7ce89cda5f8f3317b
matteo-grella <matteogrella@gmail.com>
2021-01-11 22:35:55 +0100

    Implement a Gateway to access LaBSE API (spaGO)

---
+++ pkg/tasks/vectorizer/labse/labse.go
@@ -0,0 +1,40 @@
+// Copyright 2021 WhatsNew Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package labse
+
+import (
+	"context"
+
+	spagogrpcapi "github.com/nlpodyssey/spago/pkg/nlp/transformers/bert/grpcapi"
+	"google.golang.org/grpc"
+)
+
+// Gateway provides access to the LaBSE API (spaGO).
+type Gateway struct {
+	connection *grpc.ClientConn
+}
+
+// New returns Gateway objects for accessing the LaBSE API.
+func New(connection *grpc.ClientConn) *Gateway {
+	return &Gateway{
+		connection: connection,
+	}
+}
+
+// Vectorize returns a dense representation of given text using Language-Agnostic BERT Sentence Embedding (LaBSE).
+func (s *Gateway) Vectorize(text string) ([]float32, error) {
+	client := spagogrpcapi.NewBERTClient(s.connection)
+	encoding, err := client.Encode(context.Background(), &spagogrpcapi.EncodeRequest{
+		Text: text,
+	})
+	if err != nil {
+		return []float32{}, err
+	}
+	return encoding.Vector, nil
+}
+
+func (s *Gateway) Close() error {
+	return s.connection.Close()
+}
