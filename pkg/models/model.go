a802f2b553f2857bde2b437917742f198f8afc1d
Marco Nicola <marconicola@disroot.org>
2021-07-01 18:23:31 +0200

    Create models package

---
+++ pkg/models/model.go
@@ -0,0 +1,14 @@
+// Copyright 2021 SpecializedGeneralist. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package models
+
+import "time"
+
+// Model is the basic struct embedded into all GORM models.
+type Model struct {
+	ID        uint      `gorm:"primaryKey"`
+	CreatedAt time.Time `gorm:"not null"`
+	UpdatedAt time.Time `gorm:"not null"`
+}
---
+++ pkg/models/models.go
@@ -0,0 +1,15 @@
+// Copyright 2021 SpecializedGeneralist. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package models
+
+import "gorm.io/gorm"
+
+// allModels is the list of all GORM models, used for auto-migration.
+var allModels = []interface{}{}
+
+// AutoMigrate performs the automatic migration of all GORM models.
+func AutoMigrate(db *gorm.DB) error {
+	return db.AutoMigrate(allModels...)
+}
