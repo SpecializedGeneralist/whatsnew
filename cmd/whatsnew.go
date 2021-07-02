14c94c6e8a58d346ce806b643a2a37f79a8dc6ad
Marco Nicola <marconicola@disroot.org>
2021-07-02 19:18:01 +0200

    Initialize logger from main command

--- cmd/whatsnew.go
+++ cmd/whatsnew.go
@@ -6,12 +6,26 @@ package main
 
 import (
 	"github.com/SpecializedGeneralist/whatsnew/pkg/cli"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"os"
+	"time"
 )
 
 func main() {
+	initLogger()
 	err := cli.Run(os.Args[0], os.Args[1:])
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+// Initialize the global zerolog Logger, which will be used as basis by all
+// whatsnew commands and operations.
+func initLogger() {
+	w := zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+	}
+	log.Logger = log.Output(w)
 }
