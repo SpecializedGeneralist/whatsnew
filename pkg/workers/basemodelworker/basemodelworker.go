2907ef2f2ce274de82fc8cac186956d7c3ab6b61
Marco Nicola <marconicola@disroot.org>
2021-07-09 19:16:24 +0200

    Create basemodelworker package

---
+++ pkg/workers/basemodelworker/basemodelworker.go
@@ -0,0 +1,50 @@
+// Copyright 2021 SpecializedGeneralist. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package basemodelworker
+
+import (
+	"context"
+	"fmt"
+	"github.com/contribsys/faktory_worker_go"
+	"github.com/rs/zerolog"
+	"gorm.io/gorm"
+)
+
+// Worker can be embed by specific worker implementations which
+// operate on database models.
+type Worker struct {
+	Name        string
+	DB          *gorm.DB
+	FK          *faktory_worker.Manager
+	Log         zerolog.Logger
+	Concurrency int
+	Perform     Perform
+}
+
+// Perform actually executes the job.
+type Perform func(ctx context.Context, modelID uint) error
+
+// Run sets the concurrency, registers the worker handler and starts
+// processing jobs.
+// This function never returns (refer to faktory_worker_go Manager.Run).
+func (w Worker) Run() {
+	w.FK.Concurrency = w.Concurrency
+	w.FK.Register(w.Name, w.faktoryPerform)
+	w.FK.Run()
+}
+
+func (w Worker) faktoryPerform(ctx context.Context, args ...interface{}) error {
+	if len(args) != 1 {
+		return fmt.Errorf("invalid arguments: %#v", args)
+	}
+
+	f, ok := args[0].(float64)
+	if !ok {
+		return fmt.Errorf("invalid model ID argument: %#v", args[0])
+	}
+	modelID := uint(f)
+
+	return w.Perform(ctx, modelID)
+}
