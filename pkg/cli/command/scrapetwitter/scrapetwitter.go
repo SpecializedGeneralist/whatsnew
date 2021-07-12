cf238d992e0f10b801bc6cf9f2517a82d8f02bc7
Marco Nicola <marconicola@disroot.org>
2021-07-12 20:18:26 +0200

    Implement command "scrape-twitter"

--- pkg/cli/command/scrapetwitter/scrapetwitter.go
+++ pkg/cli/command/scrapetwitter/scrapetwitter.go
@@ -8,6 +8,9 @@ import (
 	"context"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/cli/command"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/config"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/database"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/workers"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/workers/twitterscraper"
 )
 
 // CmdScrapeTwitter implements the command "whatsnew scrape-twitter".
@@ -15,11 +18,36 @@ var CmdScrapeTwitter = &command.Command{
 	Name:      "scrape-twitter",
 	UsageLine: "scrape-twitter",
 	Short:     "scrape twitter from user or search sources",
-	Long:      "", // TODO: ...
-	Run:       Run,
+	Long: `
+The command "scrape-twitter" runs the worker for fetching tweets from specific
+twitter sources.
+`,
+	Run: Run,
 }
 
 // Run runs the command "whatsnew scrape-twitter".
-func Run(ctx context.Context, conf *config.Config, args []string) error {
-	panic("not implemented")
+func Run(_ context.Context, conf *config.Config, args []string) error {
+	if len(args) != 0 {
+		return command.ErrInvalidArguments
+	}
+
+	db, err := database.OpenDB(conf.DB)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := database.CloseDB(db); e != nil && err == nil {
+			err = e
+		}
+	}()
+
+	fk, err := workers.NewManager(conf.Faktory)
+	if err != nil {
+		return err
+	}
+
+	ts := twitterscraper.New(conf.Workers.TwitterScraper, db, fk)
+	ts.Run()
+
+	return nil
 }
