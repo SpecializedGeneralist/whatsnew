646527fece106537144383d8e175900417aeea3a
Marco Nicola <marconicola@disroot.org>
2021-07-13 21:38:20 +0200

    Implement command "scrape-web"

--- pkg/cli/command/scrapeweb/scrapeweb.go
+++ pkg/cli/command/scrapeweb/scrapeweb.go
@@ -8,6 +8,9 @@ import (
 	"context"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/cli/command"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/config"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/database"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/workers"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/workers/webscraper"
 )
 
 // CmdScrapeWeb implements the command "whatsnew scrape-web".
@@ -15,11 +18,36 @@ var CmdScrapeWeb = &command.Command{
 	Name:      "scrape-web",
 	UsageLine: "scrape-web",
 	Short:     "scrape news articles from Web Resource URLs",
-	Long:      "", // TODO: ...
-	Run:       Run,
+	Long: `
+The command "scrape-web" runs the worker for scraping Web pages, creating new
+WebArticles from existing WebResources.
+`,
+	Run: Run,
 }
 
 // Run runs the command "whatsnew scrape-web".
-func Run(ctx context.Context, conf *config.Config, args []string) error {
-	panic("not implemented")
+func Run(_ context.Context, conf *config.Config, args []string) error {
+	if len(args) != 0 {
+		return command.ErrInvalidArguments
+	}
+
+	db, err := database.OpenDB(conf.DB)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := database.CloseDB(db); e != nil && err == nil {
+			err = e
+		}
+	}()
+
+	fk, err := workers.NewManager(conf.Faktory)
+	if err != nil {
+		return err
+	}
+
+	ws := webscraper.New(conf.Workers.WebScraper, db, fk)
+	ws.Run()
+
+	return nil
 }
