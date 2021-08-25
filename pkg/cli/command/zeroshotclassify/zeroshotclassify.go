528f1d52f0f5e132dddbdf6ff8146bce070a1f8f
Marco Nicola <marconicola@disroot.org>
2021-08-25 23:48:47 +0200

    Implement command "zero-shot-classify"

--- pkg/cli/command/zeroshotclassify/zeroshotclassify.go
+++ pkg/cli/command/zeroshotclassify/zeroshotclassify.go
@@ -8,18 +8,52 @@ import (
 	"context"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/cli/command"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/config"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/database"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/grpcconn"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/workers"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/workers/zeroshotclassifier"
 )
 
 // CmdZeroShotClassify implements the command "whatsnew zero-shot-classify".
 var CmdZeroShotClassify = &command.Command{
 	Name:      "zero-shot-classify",
 	UsageLine: "zero-shot-classify",
-	Short:     "classify articles with spaGO zero-shot classification service",
-	Long:      "", // TODO: ...
-	Run:       Run,
+	Short:     "classify articles with spaGO BART zero-shot classification service",
+	Long: `
+The command "zero-shot-classify" runs the worker for performing BART zero-shot
+classification of existing WebArticles.
+`,
+	Run: Run,
 }
 
 // Run runs the command "whatsnew zero-shot-classify".
 func Run(ctx context.Context, conf *config.Config, args []string) error {
-	panic("not implemented")
+	if len(args) != 0 {
+		return command.ErrInvalidArguments
+	}
+
+	db, err := database.OpenDB(conf.DB)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := database.CloseDB(db); e != nil && err == nil {
+			err = e
+		}
+	}()
+
+	bartConn, err := grpcconn.Dial(ctx, conf.Workers.ZeroShotClassifier.SpagoBARTServer)
+	if err != nil {
+		return err
+	}
+
+	fk, err := workers.NewManager(conf.Faktory)
+	if err != nil {
+		return err
+	}
+
+	zsc := zeroshotclassifier.New(conf.Workers.ZeroShotClassifier, db, bartConn, fk)
+	zsc.Run()
+
+	return nil
 }
