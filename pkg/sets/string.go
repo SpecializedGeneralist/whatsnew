353c8bfd1ef9208487ff38ad3a2a66981ad080e0
Marco Nicola <marconicola@disroot.org>
2021-08-28 16:00:32 +0200

    Add missing docs in sets package

--- pkg/sets/string.go
+++ pkg/sets/string.go
@@ -4,25 +4,33 @@
 
 package sets
 
+// StringSet is a set of unique strings.
 type StringSet map[string]emptyStruct
 
+// NewStringSet creates a new StringSet, pre-allocating a small starting size
+// of memory.
 func NewStringSet() StringSet {
 	return make(StringSet)
 }
 
+// NewStringSetWithSize creates a new StringSet, pre-allocating the specified
+// starting size of memory.
 func NewStringSetWithSize(size int) StringSet {
 	return make(StringSet, size)
 }
 
+// Has reports whether v is included in the set.
 func (s StringSet) Has(v string) bool {
 	_, ok := s[v]
 	return ok
 }
 
+// Add adds v to the set.
 func (s StringSet) Add(v string) {
 	s[v] = emptyStructValue
 }
 
+// Delete deletes v from the set.
 func (s StringSet) Delete(v string) {
 	delete(s, v)
 }
