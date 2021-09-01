3753a1bcb75cac8a38450749849456efff28d031
Marco Nicola <marconicola@disroot.org>
2021-09-01 15:29:31 +0200

    Implement command "server"

--- pkg/cli/command/server/server.go
+++ pkg/cli/command/server/server.go
@@ -8,6 +8,8 @@ import (
 	"context"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/cli/command"
 	"github.com/SpecializedGeneralist/whatsnew/pkg/config"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/database"
+	"github.com/SpecializedGeneralist/whatsnew/pkg/server"
 )
 
 // CmdServer implements the command "whatsnew server".
@@ -15,11 +17,29 @@ var CmdServer = &command.Command{
 	Name:      "server",
 	UsageLine: "server",
 	Short:     "run the API server",
-	Long:      ``, // TODO: ...
-	Run:       Run,
+	Long: `
+The command "server" runs the HTTP + gRPC server for performing basic operations
+on the database models.
+`,
+	Run: Run,
 }
 
 // Run runs the command "whatsnew server".
 func Run(ctx context.Context, conf *config.Config, args []string) error {
-	panic("not implemented")
+	if len(args) != 0 {
+		return command.ErrInvalidArguments
+	}
+
+	db, err := database.OpenDB(conf.DB)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := database.CloseDB(db); e != nil && err == nil {
+			err = e
+		}
+	}()
+
+	s := server.New(conf.Server, db)
+	return s.Run(ctx)
 }
